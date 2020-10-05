f6aae51d47cacfd5bad796666dfa414714bd05c7
LeoTheLegion <mmena2017@fau.edu>
2020-10-04 23:57:37 -0400

    final build

--- www/js/app.js
+++ www/js/app.js
@@ -3,8 +3,14 @@ var renderResult = function(){
     var htmlTemplate =  "<ul>" +
                             "<h2><<City>></h2>" +
                             "<h3><<Cloudness>></h3>" +
-                            "<li><strong>Temperature</strong> : <<Temp>> F</li>" +
-                            "<li><strong>Wind</strong> : <<Wind>> mph</li>" +
+                            "<li><strong>Temperature</strong></li>" +
+                                "<ul>" +
+                                    "<li><strong>Current</strong> : <<Temp_C>> F</li>" +
+                                    "<li><strong>Feel Like</strong> : <<Temp_feels_like>> F</li>" +
+                                    "<li><strong>Low</strong> : <<Temp_L>> F</li>" +
+                                    "<li><strong>High</strong> : <<Temp_H>> F</li>" +
+                                "</ul>" +
+                            "<li><strong>Wind</strong> : <<Wind>> mph , <<degree>> degrees</li>" +
                             "<li><strong>Humidity</strong> : <<Humidity>></li>" +
                         "</ul>" +
                         "<div id = 'map'></div>";
@@ -12,14 +18,21 @@ var renderResult = function(){
     getWeather(function(json) {
         console.log(json);
 
-        var temp_f = Math.round((json.main.temp - 273.15)*(9/5)+35);
+        var temp_f = k_to_f(json.main.temp);
+        var temp_f_high = k_to_f(json.main.temp_max);
+        var temp_f_low = k_to_f(json.main.temp_min);
+        var temp_feels_like = k_to_f(json.main.feels_like);
 
         var htmlasResult = htmlTemplate;
 
-        htmlasResult = htmlasResult.replace("<<City>>",json.name)
-        htmlasResult = htmlasResult.replace("<<Temp>>",temp_f)
+        htmlasResult = htmlasResult.replace("<<City>>",json.name +" , "+ json.sys.country)
+        htmlasResult = htmlasResult.replace("<<Temp_C>>",temp_f)
+        htmlasResult = htmlasResult.replace("<<Temp_feels_like>>",temp_feels_like)
+        htmlasResult = htmlasResult.replace("<<Temp_L>>",temp_f_low)
+        htmlasResult = htmlasResult.replace("<<Temp_H>>",temp_f_high)
         htmlasResult = htmlasResult.replace("<<Cloudness>>",json.weather[0].description)
         htmlasResult = htmlasResult.replace("<<Wind>>",json.wind.speed)
+        htmlasResult = htmlasResult.replace("<<degree>>",json.wind.deg)
         htmlasResult = htmlasResult.replace("<<Humidity>>",json.main.humidity)
         
         $('#result').html(htmlasResult);
@@ -38,6 +51,10 @@ var searchWithGPS = function(){
     function() {
         alert('Error getting location');
     });
+}
+
+var k_to_f = function(k){
+    return Math.round((k - 273.15)*(9/5)+35);
 }
 
 $(document).ready(function(){
